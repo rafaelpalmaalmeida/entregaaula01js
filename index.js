3abe8ac7374728666d101fa3f828891a20465f55
Rafael Palma Almeida <rafaelpalmaalmeida@hotmail.com>
2023-05-30 20:20:53 -0300

    Entrega aula 01

---
+++ index.js
@@ -0,0 +1,7 @@
+let anoAtual = 2023;
+let anoNascimento = prompt("Insira o ano que você nasceu!");
+let idadeDaPessoa = anoAtual - anoNascimento;
+
+alert(idadeDaPessoa);
+
+console.log(idadeDaPessoa);
